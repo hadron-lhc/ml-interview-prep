941d0f7ca9ec5cffb925fdc526af443d164586c1
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2026-04-05 20:24:55 -0300

    ejercicios de multijoins

--- 03_sql/02_multijoins/exercises/exercises.sql
+++ 03_sql/02_multijoins/exercises/exercises.sql
@@ -58,5 +58,88 @@ LEFT JOIN departments d
 ON e.department_id = d.department_id
 
 LEFT JOIN projects p
+ON d.department_id = p.department_id;
+
+
+/*
+EXERCISE 4
+
+Return:
+employee name
+department name
+project name
+
+Requirement:
+Keep employees without department.
+ */
+
+SELECT e.name, d.department_name, p.project_name
+FROM employees e
+
+LEFT JOIN departments d
+ON e.department_id = d.department_id
+
+LEFT JOIN projects p
+ON d.department_id = p.department_id;
+
+
+/*
+EXERCISE 5
+
+Return:
+department name
+number_of_projects
+number_of_employees
+
+(Hint: cuidado con COUNT)
+
+ */
+
+SELECT
+    d.department_name,
+    count(DISTINCT p.project_id) as canti_project,
+    count(DISTINCT e.employee_id) as cantidad_enployees
+FROM departments d
+
+LEFT JOIN employees e
+ON d.department_id = e.department_id
+
+JOIN projects p
 ON d.department_id = p.department_id
 
+GROUP BY d.department_name;
+
+
+/*
+EXERCISE 6
+
+Return:
+employee name
+department name
+salary
+
+Condition:
+Only employees from departments with more than 1 project.
+
+(Hint mental):
+JOIN
+GROUP
+FILTER GROUP
+JOIN BACK
+
+ */
+
+
+SELECT
+    e.name,
+    d.department_name,
+    e.salary
+FROM employees e
+LEFT JOIN departments d
+ON e.department_id = d.department_id
+WHERE e.department_id IN (
+    SELECT department_id
+    FROM projects
+    GROUP BY department_id
+    HAVING COUNT(project_id) > 1
+);
