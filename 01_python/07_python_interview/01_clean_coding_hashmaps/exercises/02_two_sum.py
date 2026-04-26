2603217aa3ad650ea9c8f37c406c467d7f1f02ce
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2026-04-26 18:26:03 -0300

    ejercicio tipico de two sum

--- 01_python/07_python_interview/01_clean_coding_hashmaps/exercises/02_two_sum.py
+++ 01_python/07_python_interview/01_clean_coding_hashmaps/exercises/02_two_sum.py
@@ -12,12 +12,21 @@ Resultado: [0,1]
 
 
 def two_sum(nums: list[int], target: int) -> list[int]:
-    pass
+    prev_map = {}
+
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in prev_map:
+            return [prev_map[diff], i]
+        prev_map[num] = i
+
+    return []
 
 
 def main():
     nums = [2, 7, 11, 15]
-    result = two_sum(nums)
+    target = 9
+    result = two_sum(nums, target)
     print(result)
 
 
