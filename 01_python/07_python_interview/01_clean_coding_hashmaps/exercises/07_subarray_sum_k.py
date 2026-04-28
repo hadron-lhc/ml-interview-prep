c46926f6d0163f7abaece36c7504c1917720c97e
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2026-04-28 18:22:37 -0300

    ejercicio de prefixsum

---
+++ 01_python/07_python_interview/01_clean_coding_hashmaps/exercises/07_subarray_sum_k.py
@@ -0,0 +1,43 @@
+"""
+Problem 7 (Hard Intro)
+
+Subarray Sum = K
+
+Dado:
+        nums = [1,1,1]
+        k = 2
+
+Cantidad de subarrays continuos cuya suma sea 2
+
+
+Resultado: 2
+
+Porque:
+        [1,1] (pos 0-1)
+        [1,1] (pos 1-2)
+"""
+
+
+def solution(nums, k):
+    count = {0: 1}
+    result = 0
+    prefix_sum = 0
+
+    for num in nums:
+        prefix_sum += num
+        result += count.get(prefix_sum - k, 0)
+        count[prefix_sum] = count.get(prefix_sum, 0) + 1
+
+    return result
+
+
+def main():
+    nums = [1, 1, 1]
+    k = 2
+
+    result = solution(nums, k)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
